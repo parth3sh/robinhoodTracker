be971df4a1dc16c00cd3e5fbaac78921d320e42e
parth3sh <parthesh152@gmail.com>
2020-05-27 16:50:54 -0400

    Add files via upload
    Prints user's portfolio and expresses percentage value of each asset, along with cash on hand.

---
+++ robin.py
@@ -0,0 +1,30 @@
+import robin_stocks
+import click
+from getpass import getpass
+
+
+def main():
+    email = input("Enter your Robinhood account email: ")
+    password = getpass("Enter your password: ")
+    return robin_stocks.authentication.login(username= email, password= password, expiresIn=30, scope='internal', by_sms=True, store_session=True)
+
+def getInfo():
+    return robin_stocks.profiles.load_basic_profile()
+
+def portfolio():
+    return robin_stocks.account.build_holdings(with_dividends=False)
+
+def parsePortfolio(portfolio):
+    for stock in portfolio:
+        print("You own $" + portfolio.get(stock).get('equity') + " of " + stock + ", which is " + portfolio.get(stock).get('percentage') + "%" + " of your portfolio.")
+
+
+def getCash():
+    cash = robin_stocks.account.build_user_profile().get('cash')
+    print("You have $" + cash + " on hand.")
+if __name__ == '__main__':
+    login = main()
+    info = getInfo()
+    portfolio = portfolio()
+    parsePortfolio(portfolio)
+    getCash()
